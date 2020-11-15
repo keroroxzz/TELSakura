4bc764fa351a183101d0a5cf9e6c1a4ffc8ca67c
keroroxzz <keroroxzz@yahoo.com.tw>
2020-11-16 01:22:28 +0800

    Why?

--- startup.sh
+++ startup.sh
@@ -1,4 +1,16 @@
-#!/bin/bash
+#!/bin/sh
+#export PATH="/opt/ros/kinetic/bin:/home/rtu/bin:/home/rtu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/local/cuda/bin:/home/rtu/tools/arduino-1.8.13:/opt/ros/kinetic/lib:/opt/ros/kinetic/share"
+#export PYTHONPATH="/home/rtu/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages"
+#export ROS_PACKAGE_PATH="/home/rtu/catkin_ws/src:/opt/ros/kinetic/share"
+#export ROS_ROOT="/opt/ros/kinetic/share/ros"
+#export ROS_ETC_DIR="/opt/ros/kinetic/etc/ros/"
+#export ROSLISP_PACKAGE_DIRECTORIES="/home/rtu/catkin_ws/devel/share/common-lisp/"
+
+echo "$PATH"
+
+echo "Start up! Wait 5 secs..."
+#sleep 1
+echo "Start!"
 
 ros_add=192.168.1.123
 
